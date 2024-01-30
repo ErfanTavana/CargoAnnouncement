2c0f67fb4a18a2445b6ddbf4e164f4b73bb5ca11
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-01-30 11:20:59 +0330

    Hashtag for permissions.py file in app admin

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -1,12 +1,17 @@
+# Permission Class: IsLoggedInAndPasswordSet
+# کلاس مجوز: IsLoggedInAndPasswordSet
+
 from rest_framework import permissions
 from .models import PasswordSetStatus
 
 class IsLoggedInAndPasswordSet(permissions.BasePermission):
     def has_permission(self, request, view):
+        # Check if the user is logged in
         # بررسی اینکه کاربر لاگین کرده
         if not request.user.is_authenticated:
             return False
 
+        # Check if the user has set their password
         # بررسی اینکه کاربر پسورد خود را تنظیم کرده
         try:
             password_set_status = PasswordSetStatus.objects.get(token=request.auth)
