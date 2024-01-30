8c488185bf3ef8be22bdd1ef828848ce34a1d6ae
Erfan Tavana <84908961+ErfanTavana@users.noreply.github.com>
2024-01-30 12:55:25 +0330

    Hashtag for urls.py file in app account

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,26 +1,27 @@
-# This is the urls.py file responsible for handling authentication-related views.
+# این فایل urls.py مسئول مدیریت آدرس‌های اینترنتی مربوط به ویوهای مربوط به احراز هویت است.
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # URL pattern for sending a verification code
+    # الگوی URL برای ارسال یک کد تایید
     path('Send_verification_code/', views.Send_verification_code, name='send_verification_code'),
 
-    # URL pattern for user registration
+    # الگوی URL برای ثبت‌نام کاربر
     path('register/', views.register, name='register'),
 
-    # URL pattern for user login
+    # الگوی URL برای ورود کاربر
     path('login/', views.login, name='login'),
 
-    # URL pattern for user logout
+    # الگوی URL برای خروج کاربر
     path('logout/', views.logout, name='logout'),
 
-    # URL pattern for handling forget password functionality
+    # الگوی URL برای پردازش فراموشی رمز عبور
     path('forget_password/', views.forget_password, name='forget_password'),
 
+    # الگوی URL برای مشاهده و ویرایش اطلاعات پروفایل کاربر
     path('profile_view/', views.profile_view, name='profile_view'),
 
+    # الگوی URL برای تغییر رمز عبور کاربر
     path('set_password/', views.set_password, name='set_password'),
-
 ]
